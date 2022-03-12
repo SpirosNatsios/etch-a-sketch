b0d0f093f6d48a608573231d6e14b7eec5a3a26c
SpirosNatsios <spyrosodnut@gmail.com>
2022-03-12 03:02:31 +0200

    added a button to change the grid

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
+function main() {
+  sqrButton.addEventListener("click", changeGrid);
+}
+
+function changeGrid(selection) {
+  selection = parseInt(prompt("Choose number of squares per side (Max 100)"));
+
+  while (selection > 100 || selection < 0) {
+    selection = parseInt(prompt("Out of limits. Choose again (0-100)"));
+  }
+
+  emptyGrid();
+  getGrid(selection);
+}
+
+function getGrid(selection) {
+  console.log(selection);
+  square.setAttribute(
+    "style",
+    `width:${600 / selection - 2}px;
+   height:${600 / selection - 2}px;
+   border: 1px solid black;`
+  );
+
+  for (let i = 0; i < selection; i++) {
+    helpDiv.appendChild(square.cloneNode(true));
+  }
+  for (let i = 0; i < selection; i++) {
+    container.appendChild(helpDiv.cloneNode(true));
+  }
+}
+
+function emptyGrid() {
+  let child = container.lastElementChild;
+  while (child) {
+    container.removeChild(child);
+    child = container.lastElementChild;
+  }
+
+  child = helpDiv.lastElementChild;
+  while (child) {
+    helpDiv.removeChild(child);
+    child = helpDiv.lastElementChild;
+  }
+}
+
 const square = document.createElement("div");
 const helpDiv = document.createElement("div");
 const container = document.querySelector(".main-container");
-//square.classList.add("squares");
+const sqrButton = document.querySelector(".squares-button");
+
 let selection = 16;
-
-// square.style.cssText = `width:"${480 / selection - 2}"`;
-// square.style.cssText = `height:${480 / selection - 2}`;
-
-square.setAttribute(
-  "style",
-  `width:${540 / selection - 2}px;
-   height:${540 / selection - 2}px;
-   border: 1px solid black;`
-);
-
-for (let i = 0; i < selection; i++) {
-  helpDiv.appendChild(square.cloneNode(true));
-}
-for (let i = 0; i < selection; i++) {
-  container.appendChild(helpDiv.cloneNode(true));
-}
+getGrid(selection);
+main(selection);
